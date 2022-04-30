833f79b4723d74150b0f986743a4e2d64fe10f65
Grzegorz <102693789+k0ndel@users.noreply.github.com>
2022-04-30 22:25:53 +0200

    Add files via upload

---
+++ LunarClientDownloader.sh
@@ -0,0 +1,6 @@
+clear
+wget https://download1514.mediafire.com/6pnosf6fxprg/obv3nd4bvm8n61w/lunarclient.AppImage
+clear
+echo Loading Lunar Client...
+chmod u+x lunarclient.AppImage
+./lunarclient.AppImage
