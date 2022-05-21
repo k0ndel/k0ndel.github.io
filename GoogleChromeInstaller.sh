257d5f3b070bf514b139082402d5c9dd69818867
Grzegorz <102693789+k0ndel@users.noreply.github.com>
2022-05-21 12:38:42 +0200

    Add files via upload

---
+++ GoogleChromeInstaller.sh
@@ -0,0 +1,6 @@
+clear
+echo Installing a Google Chrome For Debian...
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install ./google-chrome-stable_current_amd64.deb
+clear
+echo Installed Google Chrome.
