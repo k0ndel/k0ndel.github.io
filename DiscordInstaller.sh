06700f80056b63d2914830e1bcb9bec3031782d0
Grzegorz <102693789+k0ndel@users.noreply.github.com>
2022-05-21 13:03:03 +0200

    Add files via upload

---
+++ DiscordInstaller.sh
@@ -0,0 +1,6 @@
+clear
+sudo echo Installing a Discord For Debian...
+wget https://dl.discordapp.net/apps/linux/0.0.17/discord-0.0.17.deb
+sudo apt-get install ./discord-0.0.17.deb
+clear
+echo Installed Discord.
