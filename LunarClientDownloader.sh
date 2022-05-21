d822ba13a078257700d747594b0c8e357faff5ce
Grzegorz <102693789+k0ndel@users.noreply.github.com>
2022-05-21 13:02:36 +0200

    Update LunarClientDownloader.sh

--- LunarClientDownloader.sh
+++ LunarClientDownloader.sh
@@ -1,6 +1,2 @@
 clear
-wget https://download1514.mediafire.com/6pnosf6fxprg/obv3nd4bvm8n61w/lunarclient.AppImage
-clear
-echo Loading Lunar Client...
-chmod u+x lunarclient.AppImage
-./lunarclient.AppImage
+echo dosnt works
