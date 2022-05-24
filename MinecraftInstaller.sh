03c412aaf903a386ec5baad4ff2eeb643f2b144f
Grzegorz <102693789+k0ndel@users.noreply.github.com>
2022-05-24 13:55:52 +0200

    Update MinecraftInstaller.sh

--- MinecraftInstaller.sh
+++ MinecraftInstaller.sh
@@ -1,6 +1,6 @@
 clear
 sudo echo Installing a Minecraft Launcher For Debian...
 wget https://launcher.mojang.com/download/Minecraft.deb
-sudo apt-get install ./Minecraft.deb
+sudo dpkg -i ./Minecraft.deb
 clear
 echo Installed Minecraft Launcher.
